746d4d774d2c8f6bccf5b59231a771f88b030697
reinoptland <reinoptland@gmail.com>
2016-08-06 17:02:28 +0200

    Added flavor to the seed file and show template

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,13 @@
 class Player < ApplicationRecord
   belongs_to :game
   has_many :squares
+
+  def roll
+    roll = rand(6) + 1
+    player.visiting_position += roll
+  end
+  #
+  # def player_present?
+  #   visiting_position == square_id
+  # end
 end
