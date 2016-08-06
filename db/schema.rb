9a7abbe65f27fb584431a61cd300499160bfefd6
reinoptland <reinoptland@gmail.com>
2016-08-06 14:10:04 +0200

    Added game references to square, added relations between Game and Squares

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160806085554) do
+ActiveRecord::Schema.define(version: 20160806120657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 20160806085554) do
     t.integer  "position"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "game_id"
+    t.index ["game_id"], name: "index_squares_on_game_id", using: :btree
   end
 
+  add_foreign_key "squares", "games"
 end
